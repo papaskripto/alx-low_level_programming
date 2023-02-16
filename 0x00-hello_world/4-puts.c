50bc15fa5a88d3f82348612e180d0cddcbde211c
Fribuz <bygeoffreyouma@gmail.com>
2023-02-16 04:58:46 -0800

    First C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print exactly, "Programming is like building a multilingual puzzle,
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
