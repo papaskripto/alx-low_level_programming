e476d8bdf19bdaed3981692ee0f9ffe8a257811e
Fribuz <bygeoffreyouma@gmail.com>
2023-02-16 05:21:20 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints exactly, with proper grammar, but the outcome is a piece of art,
+ * followed by a new line
+ *
+ * Retun: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
