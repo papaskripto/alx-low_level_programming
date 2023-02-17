ef4741db029287f6194dee511ab0af2b552d6625
Fribuz <bygeoffreyouma@gmail.com>
2023-02-17 09:48:12 -0800

    neg0ps

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * Main - Entry point
+ * Main - assign a random number to the variable n each time it is executed
+ * and print the last digit of the number stored in the variable n
  *
  * Return: Always 0 (Success)
  */
@@ -16,11 +17,11 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
-       	else if (n == 0) 
+       	if (n == 0) 
 	{
 		printf("%d is zero\n", n);
 	}
-	else  if (n > 0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
