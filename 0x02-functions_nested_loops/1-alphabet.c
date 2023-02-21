3155f966048e46dd2d8ba46e6405b169dd17b9f8
Fribuz <bygeoffreyouma@gmail.com>
2023-02-21 10:04:43 -0800

    lowakes alfabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- *main - prints the alphabet, in lowercase, followed by a new line
+ *main - prints the alphabet,
+ * in lowercase, followed by a new line
  *Return: Always 0 (Success)
  */
 void print_alphabet(void)
