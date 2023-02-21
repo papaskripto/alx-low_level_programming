93866941d74ded425270ace5ea9104ce3b8084e3
Fribuz <bygeoffreyouma@gmail.com>
2023-02-21 00:14:21 -0800

    putchar

---
+++ 0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main -> a program that prints _putchar, followed by a new line
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar("_putchar");
+putchar('\n');
+}
