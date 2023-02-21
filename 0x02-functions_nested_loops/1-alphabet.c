8ef679ca7e6db1d12f83ee090ecba8195f3aa68f
Fribuz <bygeoffreyouma@gmail.com>
2023-02-21 08:24:57 -0800

    alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *main - prints the alphabet, in lowercase,
+ *followed by a new line.
+ *Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+		_putchar('\n');
+	}
+	return;
+}
