708d8f54c5b999c99a9e7cf331c8edceb5100ada
Fribuz <bygeoffreyouma@gmail.com>
2023-02-27 01:45:59 -0800

    prints a string to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * _puts - prints a string, followed by a new line,
- * to stdout
- * @*str: parameter to _puts function
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
  */
 void _puts(char *str)
 {
