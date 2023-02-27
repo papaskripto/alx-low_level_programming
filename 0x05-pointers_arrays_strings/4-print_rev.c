25809931130e5d9c95063cf18f6dd739a0d05fae
Fribuz <bygeoffreyouma@gmail.com>
2023-02-27 08:13:59 -0800

    reverse a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	char ouma;
+	int m, len, len1;
+
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len1 = len - 1;
+
+	for (m = 0; m < len/2; m++)
+	{
+		ouma = s[m];
+		s[m] = s[len1];
+		s[len1 --] = ouma;
+	}
+}
