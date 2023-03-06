0186f37ecf746b25caea97ce87a5c7071fb330c2
Fribuz <bygeoffreyouma@gmail.com>
2023-03-06 10:59:50 -0800

    memset is bs

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - fills memory with a constant byte
+ * @n: first bytes to be filled
+ * @s: memory area pointed to
+ * @b: the constant byte
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+
+		printf("%c\n", a);
+
+	int main()
+	{
+		int n = 10;
+		char *s;
+		s = &b;
+
+		memset(b, 0, s*sizeof(b[0]));
+		printf("Array after memset()\n");
+		printf("%d\n", n);
+
+	}
+}
